4c14132fb2036527c94bd43e851ba3972ed5bd56
y7amura <y7amura@users.noreply.github.com>
2022-10-05 11:59:07 +0000

    deploy: 4ac5512aa60056cc1667b728da0c7a68ed8fbe3c

---
+++ theme/custom.js
@@ -0,0 +1,16 @@
+// Add line numbers on code area
+window.addEventListener("load", function() {
+    hljs.initHighlighting();
+    hljs.initLineNumbersOnLoad();
+});
+
+// Open external links in new tabs
+const externalRegex = new RegExp('^(http:\\/\\/|https:\\/\\/)');
+const target = document.getElementsByTagName('main')[0];
+const elems = document.getElementsByTagName('a');
+for(let elem of elems) {
+    if(externalRegex.test(elem.getAttribute('href'))) {
+        elem.setAttribute('rel', 'external noopener noreferrer');
+        elem.setAttribute('target', '_blank');
+    }
+}
